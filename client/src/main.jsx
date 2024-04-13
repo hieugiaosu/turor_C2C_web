4f4cfdac93dcdfd4525be71bff7da22cbb843b4c
hieugiaosu <phamthehieuvtt123@gmail.com>
2024-04-13 21:46:42 +0700

    update router, example outlet

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 // import App from './App.jsx'
 import './index.css'
 import StatisticPage from './pages/admin/statistic-page/StatisticPage.jsx'
+import WebRouter from './routes/WebRouter.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* Comment for testing page */}
-    <StatisticPage/>
+    {/* <StatisticPage/> */}
+    <WebRouter></WebRouter>
   </React.StrictMode>,
 )
