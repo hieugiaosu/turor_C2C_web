1a9a9bab2cd04dfdae4597ff180be2c293445b92
hieugiaosu <phamthehieuvtt123@gmail.com>
2024-04-18 13:45:21 +0700

    add admin navigator, fix web router

--- client/src/routes/WebRouter.jsx
+++ client/src/routes/WebRouter.jsx
@@ -1,21 +1,27 @@
-import {Route, Routes, BrowserRouter} from 'react-router-dom'
-import App from '../App';
-import AdminPage from '../pages/admin/AdminPage';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import App from "../App";
+import AdminPage from "../pages/admin/AdminPage";
 
-function WebRouter(){
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<App></App>}>
-                    
-                    {/* <Route index element={<Home></Home>} /> */}
-                </Route>
-                <Route path="/admin/" element={<AdminPage></AdminPage>}>
-                    <Route index element={<></>} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
+function WebRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* main page router */}
+        <Route path="/" element={<App></App>}>
+          {/* <Route index element={<Home></Home>} /> */}
+        </Route>
+        {/* admin router */}
+        <Route path="admin" element={<AdminPage></AdminPage>}>
+          <Route path="pages" element={<></>}></Route>
+          <Route path="user_management" element={<></>}></Route>
+          <Route path="notifications" element={<></>}></Route>
+          <Route path="inbox" element={<></>}></Route>
+          <Route path="settings" element={<></>}></Route>
+          <Route index element={<></>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default WebRouter;
