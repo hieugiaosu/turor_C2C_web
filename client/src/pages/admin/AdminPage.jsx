37cf92a69daaae66a5f3fcd4398aa2380fc6650f
leanhhuy0709 <huy.leanh0709@hcmut.edu.vn>
2024-04-20 14:41:30 +0700

    fix padding error

--- client/src/pages/admin/AdminPage.jsx
+++ client/src/pages/admin/AdminPage.jsx
@@ -4,7 +4,7 @@ import AdminNavigator from "../../components/admin/navigators/Navigator";
 function AdminPage() {
   return (
     <>
-      <div className="row">
+      <div className="row padding-0">
         <AdminNavigator></AdminNavigator>
         <Outlet></Outlet>
       </div>
